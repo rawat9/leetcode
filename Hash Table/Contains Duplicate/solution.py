48b83b59ba02f0a5b956d00c45fd36d1e44b02c9
Anurag Rawat <anurag.rawat0201@gmail.com>
2021-09-05 22:03:22 +0100

    + hashtable:containsDuplicate

---
+++ Hash Table/Contains Duplicate/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def containsDuplicate(self, nums: list[int]) -> bool:
+
+		# using set() will remove duplicate elements
+        return len(nums) != len(set(nums))
+
+# Time Complexity: O(1)
