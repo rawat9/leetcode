c81979645ecf6485b1a56812eba92a35a15c9f45
Anurag Rawat <anurag.rawat0201@gmail.com>
2021-09-14 10:24:55 +0100

    + DS {Linked Lists} - [Remove duplicates]

---
+++ Linked List/Remove Duplicates from Sorted List/solution.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def deleteDuplicates(self, head):
+        first = head
+        while first and first.next:
+            if first.val == first.next.val:
+                first.next = first.next.next
+            else:
+                first = first.next
+        
+        return head
