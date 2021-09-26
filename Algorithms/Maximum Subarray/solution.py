032ab1cd069ba225e2638e9e63efde1bdcc62561
Anurag Rawat <anurag.rawat0201@gmail.com>
2021-09-26 19:41:46 +0100

    + MaxSubarray | O(n) | Kadane's Algo ⭐️

---
+++ Algorithms/Maximum Subarray/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        """
+        Kadane's Algorithm
+        """
+        max_current = max_global = nums[0]
+        
+        for i in range(1, len(nums)):
+            max_current = max(nums[i], max_current + nums[i])
+            if max_current > max_global:
+                max_global = max_current
+        return max_global
