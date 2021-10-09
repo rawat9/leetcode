955dc72999d4d5155381e8fed25aeb545dde10ac
Anurag Rawat <anurag.rawat0201@gmail.com>
2021-10-09 23:37:27 +0100

    + [BinaryTree] - InOrder Traversal | recursive ⭐️

---
+++ Algorithms/Binary Tree Inorder Traversal/solution.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        """Left -> Root -> Right"""
+
+        if not root:
+            return []
+        
+        if root:
+            return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
+            
