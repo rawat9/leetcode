4c816ad2f15fa22643515016ee7eae40069ba5af
Anurag Rawat <anurag.rawat0201@gmail.com>
2021-11-23 22:20:53 +0000

    + mergeAlt | itertools.zip_longest() | join() ✓

---
+++ Algorithms/Merge Strings Alternately/solution.py
@@ -0,0 +1,5 @@
+from itertools import zip_longest
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        return "".join(w1 + w2 for w1, w2 in zip_longest(word1, word2, fillvalue=""))
