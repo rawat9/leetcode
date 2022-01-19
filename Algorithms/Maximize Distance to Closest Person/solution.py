0115cb24e2b34d346d55d8dc525a07ff7d7242f4
Anurag Rawat <anurag.rawat0201@gmail.com>
2022-01-19 23:42:00 +0000

    + maxDistance | array ⭐️

---
+++ Algorithms/Maximize Distance to Closest Person/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxDistToClosest(self, seats: list[int]) -> int:
+        n = len(seats)
+        left, right = [n] * n, [n] * n
+        
+        for i in range(n):
+            if seats[i] == 1:
+                left[i] = 0
+            elif i > 0:
+                left[i] = left[i-1] + 1
+                
+        for i in range(n-1, -1, -1):
+            if seats[i] == 1:
+                right[i] = 0
+            elif i < n-1:
+                right[i] = right[i+1] + 1
+                
+        return max(min(left[i], right[i]) for i, seat in enumerate(seats) if not seat)
