59d8332b11e8e9680fa679d6f4ed4364fd447cb5
Anurag Rawat <anurag.rawat0201@gmail.com>
2022-01-21 21:12:58 +0000

    + minEatingSpeed | binarySearch ✅

---
+++ Algorithms/Koko Eating Bananas/solution.py
@@ -0,0 +1,20 @@
+import math
+
+class Solution:
+    def minEatingSpeed(self, piles: list[int], h: int) -> int:      
+        left = 1
+        right = max(piles)
+        
+        while left < right:
+            middle = (left + right) // 2            
+            hour_spent = 0
+            
+            for pile in piles:
+                hour_spent += math.ceil(pile / middle)
+            
+            if hour_spent <= h:
+                right = middle
+            else:
+                left = middle + 1
+    
+        return right
